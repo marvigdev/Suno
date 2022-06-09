3319907225fbdba3bfaa5b5c65e3dd3b2ebaf33e
el roberto <44546537+happendo@users.noreply.github.com>
2022-06-09 14:40:06 -0300

    heal added

--- src/main/java/commands/Heal.java
+++ src/main/java/commands/Heal.java
@@ -1,20 +1,34 @@
 package commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
 
-public class Heal implements CommandExecutor {
+public class Heal implements CommandExecutor{
+
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (!(sender instanceof Player)) return false;
+    public boolean onCommand(CommandSender Sender, Command Cmd, String Label, String[] Args) {
 
-        Player sender = (Player) sender;
+        Player p = (Player)Sender;
 
-        if (args.length == 0) {
-            se
+        if(Cmd.getName().equalsIgnoreCase("heal")){
+            if(Args.length == 0) {
+                p.sendMessage("Curado com sucesso.");
+                p.setHealth(20);
+                return true;
+            }
+            if(Args.length > 1) {
+                Player t = Bukkit.getPlayerExact(Args[0]);
+                t.setHealth(20);
+                p.sendMessage("Voce curou o " + t.getName() + " com sucesso");
+                t.sendMessage("Voce foi curado  pelo " + p.getName() + " com sucesso");
+                return true;
+            }
         }
+        return false;
     }
 }
+
+
--- src/main/java/head/Loader.java
+++ src/main/java/head/Loader.java
@@ -1,5 +1,6 @@
 package head;
 
+import commands.Heal;
 import commands.sGamemode;
 import events.CommandNotFound;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -7,7 +8,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Loader {
     public static void loadCommands(JavaPlugin plugin) {
         plugin.getCommand("gm").setExecutor(new sGamemode());
-        plugin.getCommand("commands.sGamemode").setExecutor(new sGamemode());
+        plugin.getCommand("sGamemode").setExecutor(new sGamemode());
+        plugin.getCommand("heal").setExecutor(new Heal());
     }
 
     public static void loadEvents(JavaPlugin plugin) {
